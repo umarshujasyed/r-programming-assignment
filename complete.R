a6625292495362de40c9dd9d21fbea069e60b5d1
umarshujasyed <umarshuja@live.com>
2015-05-24 10:09:02 +0500

    complete.R

---
+++ complete.R
@@ -0,0 +1,23 @@
+complete <- function(directory, id = 1:332) {
+  
+  allFiles <- list.files(path = directory, full.names = TRUE)
+  
+  selectedData <- data.frame()
+  
+  completeCases <- data.frame()
+  
+  nobs <- data.frame();
+  
+  for (i in id) { 
+  
+    selectedData <- (read.csv(allFiles[i],header=TRUE))
+  
+    nobs <- sum(complete.cases(selectedData))
+  
+    completeCases <- rbind(completeCases, data.frame(i,nobs))
+  
+  }
+  
+  completeCases
+
+}
